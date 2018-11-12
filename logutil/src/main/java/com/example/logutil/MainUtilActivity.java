13df2a6fc992ad3072c1e2e13c04842421d23eb1
Mohamed Elsayed <mohamed.compu2018@hotmail.com>
2018-11-12 10:47:57 +0200

    adding private method to public activity

--- logutil/src/main/java/com/example/logutil/MainUtilActivity.java
+++ logutil/src/main/java/com/example/logutil/MainUtilActivity.java
@@ -9,5 +9,6 @@ public class MainUtilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_umain);
+        new LogDebug().d("HHHHH");
     }
 }
